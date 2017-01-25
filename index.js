5aaa0545d8ac7733c02a5338a4353a7c89dbc36d
Nikita <nikita.vostrosablin@gmail.com>
2017-01-25 14:48:21 +0100

    Add some more attempts

--- index.js
+++ index.js
@@ -6,26 +6,23 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
+
 io.on('connection', function(socket){
 
   socket.on('name', function(data){
   io.sockets.emit('broadcast', { description: data.description });
-   socket.on('disconnect', function (msg) {
-             io.emit('disconnected',  {name: data.description});
-             });
     });
-
 
   socket.on('chat message name', function(msg){
     io.emit('chat message name',  msg);
   });
 
+  socket.on('disconnect', function(data){
+     console.log(data);
   });
 
-// socket.on('disconnect', function (data) {
-	//          io.emit('disconnected',  {});
-    //        });
-  // });
+  });
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
